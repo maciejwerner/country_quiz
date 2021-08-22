46b6a4f4e58a3356dd16a5581ab976819a2f6214
Maciej Werner <maciej22@poczta.onet.eu>
2021-08-22 18:28:49 +0200

    rebuilding app

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# country_quiz
-challenge - country quiz for dev chalenges io
+## Table of contens
+* [General info](#general-info)
+* [Features](#features)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Contact](#contact)
+
+## General info
+This project is a challenge from https://devchallenges.io/
+
+## Features
+# Challenge: Create a country quiz app using an API. Use Front-end libraries like React or Vue. Don’t look at the existing solution. Fulfill user stories below:
+
+*  User story: I can see at least 2 types of questions: a city is the capital of.. or a flag belong to country..
+*  User story: I can see select an answer
+*  User story: I can see if my answer is correct or incorrect
+*  User story: When I answer correctly, I can move on to the next question
+*  User story: When I answer incorrectly, I can see my results and try again
+*  User story: I can try again
+
+## Technologies
+Project is created with:
+* HTML
+* CSS
+* Vanilla JS
+
+## Setup
+This is a simple one-page html & vanilla js project.
+
+## Contact
+Created by [@Maciej Werner](maciej22@poczta.onet.eu) - feel free to contact me!
---
+++ src/js/main.js
@@ -0,0 +1,226 @@
+// DATA 
+const getData = async () => {
+  const URL = 'https://restcountries.eu/rest/v2/all';
+
+  try {
+    const response = await fetch(`${ URL }`);
+    
+    if(response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error(`Http: error: ${ response.status }`);
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+getData();
+
+// COUNTRY ITEM OBJECT
+const getCountryData = async () => {
+  const data = await getData();
+
+  const countryData = data.map(data => {
+    return {
+      name: data.name,
+      capital : data.capital,
+      region : data.region,
+      flag : data.flag 
+    }
+  });
+
+  return countryData;
+};
+
+// RANDOM NUMBER
+const getRandomNumber = (min = 0, max = 250) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const getRandomFourCountriesIndex = () => {
+  const randomIndexArray = [];
+
+  for (let i = 0; i <= 3; i++) {
+    randomIndexArray.push(getRandomNumber());
+  }
+
+  checkRandomArray(randomIndexArray);
+  return randomIndexArray;
+};
+
+// CHECK INDEX IN ARRAY FOR REPEATED NUMBER
+const checkRandomArray = arr => {
+  for ( let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) return getRandomFourCountriesIndex();
+    }
+  }
+};
+
+// RANDOM FOUR COUNTRIES FOR QUESTIONS
+const getRandomFourCountries = async () => {
+  const countries = await getCountryData();
+  const indexes = getRandomFourCountriesIndex();
+  const countriesArr = [];
+
+  for ( let i = 0; i < indexes.length; i++) {
+    countriesArr.push(countries[indexes[i]]);
+  };
+
+  return countriesArr;
+};
+
+//  NUMBER OF ANSWERS
+const gameSumary = {
+  allAnswers : 0,
+  correctAnswers : 0,
+};
+
+// OTHER 
+const questionBox = document.querySelector(".question-box");
+
+// CHECK RESULT
+const checkResult = () => {
+  if (gameSumary.allAnswers === 4 || gameSumary.correctAnswers === 4) {
+    setTimeout(displayEndCard, 1000);
+  } else return;
+};
+
+// RANDOM COUNTRY
+const getRandomCountry = (arr) => {
+  const randomCountry = arr[getRandomNumber(0, 3)];
+  return randomCountry;
+};
+
+const cardQuestion = (country) => {
+  const randomNr = getRandomNumber();
+
+  const template = (randomNr % 2 === 0) 
+    ?
+      `<div class="question-box--header">
+        <img src="${ country.flag }" alt="Country flag - ${ country.name }" class="img img-flag">
+        <h2 class="quiz-question" data-title="${ country.name }"> Which country does this flag belong to? </h2>
+      </div>
+      `
+    :
+    ` <div class="question-box--header">
+        <h2 class="quiz-question" data-title="${ country.name }"> ${ country.capital } is the capital of: </h2>
+      </div>
+    `
+  ;
+
+  return template;
+};
+
+// DISPLAY CART WITH  QUESTIONS
+const displayQuestionCard = async () => {
+
+  const countries = await getRandomFourCountries();
+
+  const randomCountry = await getRandomCountry(countries);
+
+  questionBox.innerHTML = "";
+  questionBox.classList.contains("quiz-winner") ?  questionBox.classList.remove("quiz-winner") : '';
+
+  const quizQuestion = cardQuestion(randomCountry);
+
+  const answersTemplate = `
+    <ul class="question-list">
+      ${
+        countries.map(answer => `
+          <li class="question-list--li">
+            <button 
+              class="btn btn-answer" 
+              type="button" 
+              data-name="${ answer.name }">
+              <span class="span span-name"> ${ answer.name } </span>
+              <span class="span span-icon"> </span>
+            </button>
+          </li>
+        `).join('')
+      }
+    </ul>
+  `;
+
+  const questionCard = `
+    <img src="./src/images/globe.svg" alt="globe" class="img img-globe" />
+    ${ quizQuestion }
+    ${ answersTemplate }
+    <button class="btn btn-next disabled"> next </button>
+  `;
+
+  questionBox.innerHTML = questionCard;
+
+  const answerBtns = document.querySelectorAll(".btn-answer");
+  
+  answerBtns.forEach(btn => {
+      btn.addEventListener("click", e => {
+        checkAnswer(e, countries, answerBtns);
+    });
+  });
+
+};
+
+// CHECK ANSWER
+const checkAnswer = (e, countries, answerBtns) => {
+  const target = e.target;
+
+  const questionName = document.querySelector(".quiz-question");
+  const questionData = questionName.dataset.title;
+  
+  const correctAnswer = target.dataset.name;
+  const spanIcon = target.querySelector(".span-icon");
+  const nextBtn = document.querySelector(".btn-next");
+
+  if (correctAnswer === questionData) {
+    target.classList.add("bg-green");
+    spanIcon.classList.add("far", "fa-check-circle");
+    ++gameSumary.allAnswers;
+    ++gameSumary.correctAnswers;
+  } else if (correctAnswer !== questionData) {
+    target.classList.add("bg-red");
+    spanIcon.classList.add("far", "fa-times-circle");
+    ++gameSumary.allAnswers;
+    
+    // FIND CORRECT ANSWER
+    const indexAnswer = countries.findIndex(({ name }) => (name === questionData));
+    const btn = [...answerBtns][indexAnswer];
+    btn.classList.add("bg-green");
+    btn.querySelector(".span-icon").classList.add("far", "fa-check-circle");
+  }
+
+  answerBtns.forEach(btn => btn.setAttribute("disabled", "true"));
+  checkResult();
+
+  (gameSumary.allAnswers === 4 || gameSumary.correctAnswers === 4) ? '' : nextBtn.classList.remove("disabled");
+
+  nextBtn.addEventListener("click", displayQuestionCard);
+};
+
+// DISPLAY END CARD
+const displayEndCard = () => {
+
+  questionBox.innerHTML = "";
+  questionBox.classList.add("quiz-winner");
+
+  const endCard = `
+    <img src="./src/images/winners.svg" alt="gold trophy" class="img img-trophy">
+    <h2 class="result-title"> Results </h2>
+    <h4 class="result-desc"> You got <span class="span span-result"> ${ gameSumary.correctAnswers } </span> correct answers</h4>
+    <button class="btn btn-again"> Try again </button>
+  `;
+ 
+  questionBox.innerHTML = endCard;
+  document.querySelector(".btn-again").addEventListener("click", newGame);
+};
+
+// NEW GAME
+const newGame = () => {
+  gameSumary.allAnswers = 0;
+  gameSumary.correctAnswers = 0;
+  
+  displayQuestionCard();
+};
+newGame();
